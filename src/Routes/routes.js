f30ca3bf54d5a79af86f6ec394c375127fcaea79
agusahumada <agustinaahumada649@gmail.com>
2021-07-28 22:36:11 -0300

    Tercer ejercicio (Saludo)

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -4,18 +4,24 @@ const fs = require('fs').promises;
 module.exports = function() {
 
     // ENDPOINT FECHA
-    router.get('/date',(req,res)=>{
+    router.get('/date', (req,res) => {
         res.send(`Buenos días, la fecha de hoy es ${new Date()}`).status(200);
     });
 
     // ENDPOINT TXT
-    router.get('/text', async(req,res)=>{
+    router.get('/text', async(req,res) => {
         try {
             res.send(await fs.readFile('./lorem.txt', 'utf-8')).status(200);
         } catch (error) {
             res.send(error).status(500);
         }
     })
+
+    // ENDPOINT SALUDO
+    router.post('/greeting', (req,res) => {
+        const { name } = req.body;
+        res.send(`Buenos días ${name}!`).status(200);
+    });
 
     return router;
 }
--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ const app = express();
 
 const PORT = 3000;
 
+app.use(express.json());
+
 app.use("/", routes());
 
 app.listen(PORT, () => {
