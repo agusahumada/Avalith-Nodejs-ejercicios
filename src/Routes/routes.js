953a160a2f3bb3af12445d9514eedfbdd7d2e69c
agusahumada <agustinaahumada649@gmail.com>
2021-07-28 21:47:12 -0300

    Segundo ejercicio (texto)

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const fs = require('fs').promises;
 
 module.exports = function() {
 
@@ -6,6 +7,15 @@ module.exports = function() {
     router.get('/date',(req,res)=>{
         res.send(`Buenos días, la fecha de hoy es ${new Date()}`).status(200);
     });
+
+    // ENDPOINT TXT
+    router.get('/text', async(req,res)=>{
+        try {
+            res.send(await fs.readFile('./lorem.txt', 'utf-8')).status(200);
+        } catch (error) {
+            res.send(error).status(500);
+        }
+    })
 
     return router;
 }
